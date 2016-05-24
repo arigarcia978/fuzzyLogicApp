9f0248d399683385156ecec3a4d564f824ddd999
arigarcia978 <arigarcia.978@gmail.com>
2016-05-24 18:46:51 -0300

    Intentando

--- www/js/services/motorMatematico.js
+++ www/js/services/motorMatematico.js
@@ -2,4 +2,4 @@ function MotorMatematico() {}
 
 MotorMatematico.prototype.calcularDistancia = function(ubicacion1, ubicacion2){}
 
-MotorMatematico.prototype.calcularDiferenciaDeTiempoEnMinutos = function(tiempo1, tiempo2){}
+MotorMatematico.prototype.calcularDiferenciaDeTiempoEnMinutos = function(tiempo1, tiempo2){} 
