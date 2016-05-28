228b0c334df5405a1636d63ccf1e763eb0696f09
Franco Colletti <eldtctiv@gmail.com>
2016-05-28 00:19:07 -0300

    Dando inicio a la nueva rama experimental

--- www/js/services/googleMapsService.js
+++ www/js/services/googleMapsService.js
@@ -4,14 +4,14 @@
 	GoogleMapsService.prototype.getLugaresCercanos = function(ubicacion){}
 
 	GoogleMapsService.prototype.getUbicacion = function(){
-				 navigator.geolocation.getCurrentPosition(function (pos) {
-	                    var latitud= pos.coords.latitude;
-	                    var longitud= pos.coords.longitude;
-	                    //console.log('longitud = ' + latitud + ',  longitud =' + longitud);
-	                    var a = new Ubicacion(latitud, longitud, 5);
-	                    return a;
-	                })
-			}
+			 navigator.geolocation.getCurrentPosition(function (pos) {
+                    var latitud= pos.coords.latitude;
+                    var longitud= pos.coords.longitude;
+                    //console.log('longitud = ' + latitud + ',  longitud =' + longitud);
+                    var a = new Ubicacion(latitud, longitud, 5);
+                    return a;
+                })
+		}
 */ 
 
 var NEGOCIOS = {
